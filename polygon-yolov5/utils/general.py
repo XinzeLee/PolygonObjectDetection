c6ab062e8aa08d568f0f59b53464098d01e6909c
xinzelee <87064748+XinzeLee@users.noreply.github.com>
2021-07-16 18:02:00 +0800

    Update general.py

--- polygon-yolov5/utils/general.py
+++ polygon-yolov5/utils/general.py
@@ -963,7 +963,7 @@ def polygon_non_max_suppression(prediction, conf_thres=0.25, iou_thres=0.45, cla
     # Settings
     min_wh, max_wh = 3, 4096  # (pixels) minimum and maximum box width and height
     max_nms = 30000  # maximum number of boxes into polygon_nms_kernel, can increase this value
-    time_limit = 90.0  # seconds to quit after
+    time_limit = 10.0  # seconds to quit after
     redundant = True  # require redundant detections
     multi_label &= nc > 1  # multiple labels per box (adds 0.5ms/img)
     merge = False  # use merge-NMS
